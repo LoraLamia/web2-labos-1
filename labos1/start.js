324dfdb9356ab4fc232e5bfb3a752a2838ac5147
Lora Zubic <lorazubic@Loras-MacBook-Pro.local>
2023-10-20 09:48:13 +0200

    Wip

---
+++ labos1/start.js
@@ -0,0 +1,35 @@
+var express = require('express')
+var indexRouter = require('./routes/index.js')
+const { auth } = require('express-openid-connect')
+require('dotenv').config()
+
+const config = {
+    authRequired: false,
+    auth0Logout: true,
+    secret: process.env.SECRET,
+    baseURL: process.env.BASEURL,
+    clientID: process.env.CLIENTID,
+    issuerBaseURL: process.env.ISSUER
+};
+
+var app = express()
+app.set('views', 'views')
+//direktorij views
+app.set('view engine', 'ejs')
+//za viewove koristimo ejs
+app.use(express.json())
+//to je da svaki json koji dode moze biti hendlan (middlewear za dolazne podatke)
+app.use(express.urlencoded({extended:true}))
+//ovo sluzi da zahtjev koji ima query parametre moze biti hendlan (isto middlewear)
+//"extended:true" znaci da mozemo hendlat ugnijezdene podatke
+app.use(express.static('public'))
+//za posluzivanje statickih stvari koje se nalaze u direktoriju 'public' prije ikakvih ruta(slike,...)
+
+app.use(auth(config));
+//AUTH!!!
+
+app.use('/', indexRouter)
+
+app.listen(3030, () => {
+    console.log("express is running")
+})
