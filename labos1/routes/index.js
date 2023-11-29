f450d40d1510338b4c388d38a0f653b25683062e
Lora Zubic <lorazubic@Loras-MacBook-Pro.local>
2023-11-29 16:00:32 +0100

    l

--- labos1/routes/index.js
+++ labos1/routes/index.js
@@ -26,4 +26,25 @@ router.get('/', async (req, res) => {
     }
 });
 
+router.get('/callback', async (req, res) => {
+    try {
+        const snapshot = await db.collection("competitions").get();
+        const competitionsFromDatabase = [];
+        snapshot.forEach((doc) => {
+            competitionsFromDatabase.push(doc.data());
+        });
+        console.log(competitionsFromDatabase[0]["extraKljuc"]);
+
+
+        res.render('index', {
+            title: 'Competitions',
+            competitions: competitionsFromDatabase[0]["extraKljuc"],
+            isAuthenticated: req.oidc.isAuthenticated(),
+            user: req.oidc.user
+        });
+    } catch {
+
+    }
+});
+
 export const indexRouter = router;
